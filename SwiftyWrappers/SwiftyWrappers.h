641b7391d37c66d566327ed8129effb48dffa5f6
TAI VUONG <vhuutai@gmail.com>
2020-01-16 11:23:10 +0700

    Init commit

---
+++ SwiftyWrappers/SwiftyWrappers.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyWrappers.h
+//  SwiftyWrappers
+//
+//  Created by TAI VUONG on 1/16/20.
+//  Copyright © 2020 TAI VUONG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyWrappers.
+FOUNDATION_EXPORT double SwiftyWrappersVersionNumber;
+
+//! Project version string for SwiftyWrappers.
+FOUNDATION_EXPORT const unsigned char SwiftyWrappersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyWrappers/PublicHeader.h>
+
+
